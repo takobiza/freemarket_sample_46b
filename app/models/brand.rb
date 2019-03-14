4bb366e2ba78f487e4a0fb24f817fc2f652c8f92
kwtck <blues_1002@softbank.ne.jp>
2019-03-14 15:48:31 +0900

    アソシエーションの記述
    brand、category、product、product_imageの各モデルに定義

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,3 @@
 class Brand < ApplicationRecord
+  has_mamy: products, dependent: :destroy
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  has_many: product_images, dependent: :destroy
+  belongs_to: category
+  belongs_to: brand
 end
