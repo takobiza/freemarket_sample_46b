7d158acd01f01049cb5b7b502fd1c7651a390e49
kwtck <blues_1002@softbank.ne.jp>
2019-03-14 20:56:23 +0900

    トップページの商品一覧実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,14 @@
 class ProductsController < ApplicationController
   def index
+
+    @items_ladies = Category.where(large: "レディース").order("RAND()").limit(4).map{ |category| Product.find_by(category_id: category.id)}
+    @items_mens = Category.where(large: "メンズ").order("RAND()").limit(4).map{ |category| Product.find_by(category_id: category.id)}
+    @items_kids = Category.where(large: "ベビー・キッズ").order("RAND()").limit(4).map{ |category| Product.find_by(category_id: category.id)}
+    @items_cosmetics = Category.where(large: "コスメ・香水・美容").order("RAND()").limit(4).map{ |category| Product.find_by(category_id: category.id)}
+
+    @items_chanel = Product.where(brand_id: '1').order("RAND()").limit(4)
+    @items_vuitton = Product.where(brand_id: '2').order("RAND()").limit(4)
+    @items_supreme = Product.where(brand_id: '3').order("RAND()").limit(4)
+    @items_nike = Product.where(brand_id: '4').order("RAND()").limit(4)
   end
 end
