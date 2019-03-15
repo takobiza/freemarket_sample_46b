d21f1d0f35b47cccd000330f683760aeb8a15765
kwtck <blues_1002@softbank.ne.jp>
2019-03-15 17:51:33 +0900

    テストツールの導入とproducts/indexのテスト
    Rspec、factory_botを導入し、productsコントローラ内のindexアクションが正常にビューを呼び出せるかをテスト

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
   def index
-
     @items_ladies = category_search("レディース")
     @items_mens = category_search("メンズ")
     @items_kids = category_search("ベビー・キッズ")
@@ -13,7 +12,7 @@ class ProductsController < ApplicationController
   end
 end
 
-def private
+
   def category_search(category_name)
     Category.where(large: category_name).order("RAND()").limit(4).map{ |category| Product.find_by(category_id: category.id)}
   end
@@ -21,4 +20,3 @@ def private
   def brand_search(brand_id)
     Product.where(brand_id: brand_id).order("RAND()").limit(4)
   end
-end
