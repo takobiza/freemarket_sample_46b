4ff95e9f9e2db936768c9db0d3b7b96cd333e2ad
kwtck <blues_1002@softbank.ne.jp>
2019-03-17 15:08:30 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   resources :users
-
+  resources :products, only:[:show, :index]
   get '/logout', to: 'users#logout'
 end
