57b37ff63488fd2ce1a62136de707b896a770f35
kwtck <blues_1002@softbank.ne.jp>
2019-03-17 15:59:04 +0900

    Item-box内のクラスをBEM記法で書き換え

--- app/assets/javascripts/product_detail.js
+++ app/assets/javascripts/product_detail.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function(){
 $(function(){
-    $('.item-main__sub-img li').hover(function(){
-        var index = $('.item-main__sub-img li').index(this);
-        var imageurl = $('.item-main__sub-img li').eq(index).find('img').attr('src');
-        $('.item-main__main-img__img').attr('src', imageurl);
+    $('.item-box__item-main--sub-img li').hover(function(){
+        var index = $('.item-box__item-main--sub-img li').index(this);
+        var imageurl = $('.item-box__item-main--sub-img li').eq(index).find('img').attr('src');
+        $('.active-img').attr('src', imageurl);
     });
 });
 });
