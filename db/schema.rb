2748539e1bb3b27d2e9f747d76ddb47e4e8aa0af
kwtck <blues_1002@softbank.ne.jp>
2019-03-19 18:29:28 +0900

    commentsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_074529) do
+ActiveRecord::Schema.define(version: 2019_03_19_085028) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 2019_03_19_074529) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["large"], name: "index_categories_on_large"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "message", null: false
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "delivary_options", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
