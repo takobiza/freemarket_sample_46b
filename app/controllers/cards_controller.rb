78e7fa92166438014cdf3ee6e730412b6290e105
kwtck <blues_1002@softbank.ne.jp>
2019-03-26 21:12:32 +0900

    クレジットカード登録機能の実装
    payjpを使用してクレジットカード情報をDBに登録する

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,25 @@
 class CardsController < ApplicationController
+    protect_from_forgery except: :create
+
   def index
   end
 
+  def new
+  end
 
+
+  def update
+
+    Payjp.api_key = 'sk_test_cf22f826afb6315d068a24b2'
+    customer = Payjp::Customer.create(card: params[:payjpToken])
+    @user = User.find(params[:id])
+    @new_card = @user.update(pay_id: customer.id)
+    if @payment.save
+      redirect_to root_path
+    else
+      redirect_to new_user_card_path
+    end
+  end
 end
+
+
