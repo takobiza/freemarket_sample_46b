f7239658cbb6098d5bfea00c6a41daae4d959e5b
kwtck <blues_1002@softbank.ne.jp>
2019-03-26 21:08:24 +0900

    バリデーションの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   has_one :user_address, dependent: :destroy
   has_many :products
-  validates :password,               length: {minimum:6 ,message: 'は6文字以上128文字以下で入力してください'}, if: ->(u) { u.password.blank? }
+
   attr_writer :card_number, :credit_year, :credit_month, :card_code
 
   def card_number
