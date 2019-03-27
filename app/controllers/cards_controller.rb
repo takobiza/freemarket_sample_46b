e884afeca48fa0f12f0ce022064a054da899d662
kwtck <blues_1002@softbank.ne.jp>
2019-03-27 21:47:24 +0900

    クレジットカード情報登録機能実装
    payjpTokenの取得が可能になった

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,26 +1,25 @@
 class CardsController < ApplicationController
-  protect_from_forgery except: :create
+    protect_from_forgery except: :create
 
   def index
   end
 
   def new
-    @user = User.find(params[:user_id])
   end
 
 
-  def create
+  def update
+
     Payjp.api_key = 'sk_test_cf22f826afb6315d068a24b2'
     customer = Payjp::Customer.create(card: params[:payjpToken])
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
     @new_card = @user.update(pay_id: customer.id)
-
-    if @new_card != nil
+    if @payment.save
       redirect_to root_path
     else
       redirect_to new_user_card_path
     end
   end
-
 end
+
 
