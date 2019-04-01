88975dface2bc0aa30ff8106fe19af6a12e53808
kwtck <blues_1002@softbank.ne.jp>
2019-04-01 19:38:21 +0900

    conflictの解消

--- db/schema.rb
+++ db/schema.rb
@@ -69,15 +69,7 @@ ActiveRecord::Schema.define(version: 2019_03_27_080453) do
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["name"], name: "index_products_on_name"
-  end
-
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "uid"
-    t.string "provider"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
