a49d0a01d9ffb7b8448df14d8c6a33410035d59e
keisaka <kesuike.motosaka@gmail.com>
2019-04-03 13:59:29 +0900

    conflict

--- config/routes.rb
+++ config/routes.rb
@@ -35,12 +35,7 @@ Rails.application.routes.draw do
     end
   end
 
-<<<<<<< HEAD
-  resources :products, only:[:show, :index, :create, :edit, :update, :destroy] do
-=======
-
   resources :products do
->>>>>>> takobiza/master
     collection do
       get "/:product_id/rate" => "purchase#edit", as: :rate
       patch "/:product_id/rate" => "purchase#update", as: :rate_update
