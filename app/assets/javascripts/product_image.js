83ddbcc5028f3571edfbcbac218d7f2a972c342e
chang-k <necessary.sone@gmail.com>
2019-04-03 15:57:20 +0900

    Resolve bug image multi upload

--- app/assets/javascripts/product_image.js
+++ app/assets/javascripts/product_image.js
@@ -3,6 +3,24 @@ $(document).on('turbolinks:load', function() {
   $('.single-main__sell-registration__upload-drop-box__text').on('click', function() {
     var i = $('img.single-main__sell-registration__upload-drop-box__photo__image-area__image').length;
 
+    if ($('#file-photo-' + i)[0].files[0] !== undefined) {
+      // inputにfileが入っていたら以下を実行
+      var i = 0;
+      if ($('#file-photo-' + i)[0].files[0] !== undefined) {
+        var i = 1;
+        if ($('#file-photo-' + i)[0].files[0] !== undefined) {
+          var i = 2;
+          if ($('#file-photo-' + i)[0].files[0] !== undefined) {
+            var i = 3;
+            if ($('#file-photo-' + i)[0].files[0] !== undefined) {
+              var i = 4;
+            }
+          }
+        }
+      }
+      $('.single-main__sell-registration__upload-drop-box__text').attr('for', 'file-photo-' + i);
+    }
+
 
     $('#file-photo-' + i).off().on('change',function() {
       var file_photo = $(this);
