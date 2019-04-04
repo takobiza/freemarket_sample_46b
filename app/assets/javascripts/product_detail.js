e86a4e9cff48f7f01cc8493b8f189cdc3a8ade99
keisaka <kesuike.motosaka@gmail.com>
2019-04-04 20:58:23 +0900

    モーダル画面の実装

--- app/assets/javascripts/product_detail.js
+++ app/assets/javascripts/product_detail.js
@@ -5,5 +5,23 @@ $(document).on('turbolinks:load', function() {
         var imageurl = $('.item-box__item-main--sub-img li').eq(index).find('img').attr('src');
         $('.active-img').attr('src', imageurl);
     });
-});
+
+  $("dd").hide();
+  $("body").append(`<div id='glayLayer'></div>
+                      <div id='overLayer'></div>`);
+  $("#glayLayer").on("click", function(){
+    $(this).fadeOut();
+    $("#overLayer").fadeOut();
+  });
+
+  $(".item-box__delete").on("click",function(){
+    $("#glayLayer").fadeIn();
+    $("#overLayer").fadeIn().html($("+dd",this).html()).css({"margin-top":"-"+$("#overLayer").height()/2+"px", "margin-left":"-"+$("#overLayer").width()/2+"px"
+      });
+    });
+
+
+
+  });
+
 
